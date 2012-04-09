5690c09bbf40791b4f0fec2bc5ad895449105953
Edmund Smith <eddysmundo@hotmail.co.uk>
2012-04-09 23:39:31 +0100

    Changed Version number

--- DCPU-16/About.Designer.cs
+++ DCPU-16/About.Designer.cs
@@ -98,7 +98,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(271, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "0.1 (Early stages)";
+            this.labelVersion.Text = "0.1.5 (Early Assembler)";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCopyright
